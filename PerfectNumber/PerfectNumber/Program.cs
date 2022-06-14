87c6b74cbc8f94f48ea7bb76d249b8a1541389ba
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-14 13:52:36 +0530

    Commiting perfect number

---
+++ PerfectNumber/PerfectNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PerfectNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int number = Int32.Parse(Console.ReadLine());
+            int sum = 0;
+            for(int i=1;i<number;i++)
+            {
+                if(number%i == 0)
+                {
+                    sum = sum + i;
+                    
+                }
+            }
+            
+            if(sum == number)
+            {
+                Console.WriteLine("Number is a perfect number");
+            }
+            else
+            {
+                Console.WriteLine("Number is not a perfect number");
+            }
+        }
+    }
+}
