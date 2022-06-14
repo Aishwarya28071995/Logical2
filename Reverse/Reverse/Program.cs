9c9c8124b208d7eb1128db23badb5b8a8e15e09d
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-14 14:04:24 +0530

    Commiting reverse program

---
+++ Reverse/Reverse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int number = Int32.Parse(Console.ReadLine());
+            int remainder;
+            int reverse=0;
+            while(number>0)
+            {
+                remainder = number % 10;
+                reverse = reverse * 10 + remainder;
+                number = number / 10;
+            }
+
+            Console.WriteLine("The reverse of the number is " + reverse);
+        }
+    }
+}
