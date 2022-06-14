8f2640f570876f91a156a3f9a343b3c694aee72d
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-14 14:00:25 +0530

    Commiting prime number

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int number = Int32.Parse(Console.ReadLine());
+            for(int i=2;i<number;i++)
+            {
+                if(number % i == 0)
+                {
+                    Console.WriteLine("Number is not a prime number");
+                }
+                else
+                {
+                    Console.WriteLine("number is a prime number");
+                }
+            }
+        }
+    }
+}
